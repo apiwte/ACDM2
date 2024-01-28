6910673b049bd169619ec3c624007d5702e77bd6
apiwte <apiwte@gmail.com>
2024-01-28 21:01:12 +0700

    first commit

---
+++ Server2.js
@@ -0,0 +1,193 @@
+const { response } = require('express');
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var bodyParser = require('body-parser')
+const { MongoClient, Int32 } = require("mongodb");
+const { default: mongoose } = require('mongoose');
+require('dotenv').config();
+
+var jsonParser = bodyParser.json()
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+let data
+
+
+
+http.listen(3000,()=> {
+
+  console.log('port 3000 running')
+
+})
+
+app.set('view engine', 'ejs');
+
+
+// MongoDB connection URI
+const MONGODB_URI = (process.env.MONGODB_CONNECTION_STRING); // Update with your database name
+const client = new MongoClient(process.env.MONGODB_CONNECTION_STRING);
+
+// Connecting to MongoDB
+
+MongoClient.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(client => {
+    console.log('Connected to MongoDB');
+    const db = client.db('usm');
+    const collection = db.collection('acdm'); // Update with your collection name
+
+    const flightschema = {
+        airlines: String,
+        EOBT: String,
+        FlightNumber: Number,
+        STD: String
+
+
+        
+    }
+
+    const flights = mongoose.model('ACDM',flightschema)
+
+    // Define a route to fetch data from MongoDB
+
+    
+    app.get('/', async (req, res) => {
+      try {
+        
+        data = await collection.find().toArray(); // Retrieve all documents
+
+        //const data = await flights.find({});
+        //console.log(data)
+        
+        res.render(__dirname + '/views/index.ejs', { data });
+
+      } catch (err) {
+        console.error('Error retrieving data:', err);
+        res.status(500).send('Error retrieving data');
+      }
+    });
+
+    app.get('/edit/:thisid', async (req, res) => {
+      try {
+
+        var ObjectId = require('mongodb').ObjectId; 
+        var id = req.params.thisid;       
+        var o_id = new ObjectId(id);
+        
+        myId = req.params.thisid
+
+        dataEdit = await collection.find( {_id:o_id}  ).toArray(); // Retrieve data by id
+
+
+        console.log(" dataEdit : ",dataEdit)
+        
+        res.render(__dirname + '/views/edit.ejs', { dataEdit });
+
+      } catch (err) {
+        console.error('Error retrieving data:', err);
+        res.status(500).send('Error retrieving data');
+      }
+    });
+    
+    app.post('/update', urlencodedParser, async (req, res) => {
+      try {
+
+        //const formData = req.body.Flight; // Access form data from the request body
+        //console.log('Form Data:', formData);
+
+        const update_id = req.body.update_id
+        dataNew = {
+
+            FlightNumber: req.body.FlightNumber
+
+        }
+
+
+        console.log(dataNew)
+        res.redirect('/');
+
+
+        //const { FlightNumber } = req.body;
+        //console.log(req.body);
+    
+        /*
+        const updatedPerson = await Person.findOneAndUpdate(
+          { _id: req.params.id },
+          { $set: { FlightNumber } },
+          { new: true }
+        );*/
+
+        //res.redirect('/');
+      } catch (error) {
+        console.error('Error updating data:', error);
+        res.status(500).send('Internal Server Error');
+      }
+    });
+
+    
+
+  })
+  .catch(err => console.error('Error connecting to MongoDB:', err));
+
+
+
+
+  async function run() {
+
+
+    try {
+  
+      await client.connect();
+      const database = client.db('usm');
+      const messages = database.collection('acdm');
+
+
+
+
+       // open a Change Stream on the "messages" collection
+       changeStream = messages.watch();
+
+       // set up a listener when change events are emitted
+       changeStream.on("change", next => {
+           // process any change event
+           switch (next.operationType) {
+               case 'insert':
+                   console.log(next.fullDocument);
+                   console.log('inserted')
+      
+
+
+                   break;
+               case 'update':
+                   //console.log(next.updateDescription.updatedFields);
+                   //console.log('updated')
+
+                   var updated = 'Data is updated.Please refresh.'
+
+                   io.emit('acdmData',updated)
+
+                   //const jsonString = JSON.stringify(next._id);
+
+                   //io.emit('acdmData',jsonString)
+                   //console.log('Data Updated')
+                   //console.log(next._id)
+                   
+
+           }
+       });
+  
+    } catch {
+  
+      // Ensures that the client will close when you error
+      await client.close();
+    }
+  }
+  
+  run().catch(console.dir);
+
+
+
+
+
+
+
