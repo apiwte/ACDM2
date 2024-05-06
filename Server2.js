9af29655a0920ba8657dc0c00b377d7e43c40230
apiwte <157968834+apiwte@users.noreply.github.com>
2024-05-06 09:23:33 +0700

    Update Server2.js

--- Server2.js
+++ Server2.js
@@ -167,6 +167,7 @@ MongoClient.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: tr
           Dest: body.Dest,
           date_: req.body.date_,
           SOBT: body.SOBT
+          EOBT: body.SOBT
         
       
         })
